221d92ded639e1f00ba75b00eca8a60894f618e3
rerdao <reveriontech@gmail.com>
2025-04-02 12:32:21 +0800

    btn_change

--- src/Components/Answer/FinishMessage.tsx
+++ src/Components/Answer/FinishMessage.tsx
@@ -3,9 +3,14 @@ import './Answer.css'
 export default function FinishMessage() {
   return (
     <div className='finish-message'>
-      Test is Over!
-      <br />
-      <button className='finish-message-button'>
+      <div className='flex flex-col items-center justify-center'>
+        <h3>Test is Over!</h3>
+        <p>
+          Thank you for participating in the test!
+        </p>
+      </div>
+      
+      <button className='bg-blue-500 text-white px-4 py-2 rounded-md'>
         <a href='https://waste2earn.xyz'>
           Return to the main page
         </a>
